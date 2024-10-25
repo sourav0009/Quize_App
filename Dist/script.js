8ad96d267e04b8ce8c4cb79a75f254aea40a66f5
Sourav nandi <118915260+sourav0009@users.noreply.github.com>
2024-10-25 20:38:58 +0530

    1question add

---
+++ Dist/script.js
@@ -0,0 +1,11 @@
+const question = [
+    {
+        question:"Which of the following is not a Java features?",
+        answere: [
+            {text: "Dynamic", correct: false},
+            {text: "Architecture Neutral", correct: false},
+            {text: "Use of pointers", correct: false},
+            {text: "Object-oriented", correct: true},
+        ]
+    },
+]
