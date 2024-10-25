568e34b6a56ff59cd990b0fc768ffe991c19aa8f
Sourav nandi <118915260+sourav0009@users.noreply.github.com>
2024-10-25 20:57:50 +0530

    function

--- Dist/script.js
+++ Dist/script.js
@@ -40,3 +40,14 @@ const question = [
 const questionElement = document.getElementById("question");
 const answerButton = document.getElementById("answer-button");
 const nextButton = document.getElementById("next-btn");
+
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuize (){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next"
+    showQuestion();
+}
